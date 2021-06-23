7ad1222b852c0a567deacd5d76fb196dd8590c8a
zekiLove <86239243+zekiLove@users.noreply.github.com>
2021-06-23 11:58:27 +0800

    Update 38.java

--- leetcode/solved/38.java
+++ leetcode/solved/38.java
@@ -12,6 +12,40 @@ import java.util.Set;
 
 import org.junit.Test;
 
+//另外一种解法（更加快捷）
+//class Solution {
+//	List<String> list = new ArrayList<String>();
+//	boolean vis[];
+//	public String[] permutation(String s) {
+//		int n=s.length();
+//		char ch[] = s.toCharArray();
+//		//这个排序很关键，它让相同的字符相邻了
+//		Arrays.sort(ch);
+//		vis = new boolean[n];
+//		backTrack(0,n,ch,new StringBuilder());
+//		return list.toArray(new String[n]);	
+//	}
+//	
+//	public void backTrack(int i,int n,char arr[],StringBuilder sb) {
+//		if(i==n) {
+//			list.add(sb.toString());
+//			return;
+//		}
+//		
+//		for(int j=0;j<n;j++) {
+//			//这里因为已经遍历过重复的字符了，所以之后vis[x]被设置为false了，所以可以用 !vis[j]来确保下次不会遍历到它
+//			if(vis[j] || (j>0 && !vis[j-1] && arr[j]==arr[j-1])) {
+//				continue;
+//			}
+//			vis[j]=true;
+//			sb.append(arr[j]);
+//			backTrack(i+1,n,arr,sb);
+//			sb.delete(sb.length()-1,sb.length());
+//			vis[j]=false;
+//		}
+//	}
+//}
+
 class Solution {
 	String s;
 	String str[];
