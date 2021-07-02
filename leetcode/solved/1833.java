1297023f9526512e2e82db6e89b8dc030b9bff43
zekiLove <86239243+zekiLove@users.noreply.github.com>
2021-07-02 18:12:50 +0800

    Update 1833.java

--- leetcode/solved/1833.java
+++ leetcode/solved/1833.java
@@ -1,4 +1,22 @@
 class Solution {
+    //另外一种解法   
+ //	public int maxIceCream(int[] costs, int coins) {
+//		int arr[] = new int[100001];
+//		for (int cost : costs) {
+//			arr[cost]++;
+//		}
+//		int sum = 0;
+//
+//		for (int i = 1; i < 100001; i++) {
+//			if (coins >= i) {
+//				int curCount = Math.min(arr[i], coins / i);
+//				coins -= i * curCount;
+//				sum += curCount;
+//			}
+//
+//		}
+//		return sum;
+//	}
     public int maxIceCream(int[] costs, int coins) {
     	Arrays.sort(costs);
         int money=0;
